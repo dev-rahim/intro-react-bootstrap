a5d48e70b51a942064ce108ef9e5a9820f5505c6
dev-rahim <rahimuddin5915@gmail.com>
2022-10-20 17:55:02 +0600

    updated readme file

--- src/Components/NewsContainer/NewsContainer.js
+++ src/Components/NewsContainer/NewsContainer.js
@@ -7,16 +7,16 @@ const NewsContainer = () => {
 
     const [allNews, setAllNews] = useState([]);
     useEffect((() => {
-        fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8679413729d040cf85e3be5c72f1e4eb')
-            // fetch('./newsData.json')
+        // fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8679413729d040cf85e3be5c72f1e4eb')
+        fetch('./newsData.json')
             .then(res => res.json())
             .then(data => setAllNews(data.articles))
     }), [])
     return (
         <Row xs={1} md={3} className="g-4 mt-5 text-center">
             {
-                allNews.length === 0 ? <div className='spinner'><Spinner animation="border" /></div>
-                    : allNews.map(news => <News news={news} />)
+                allNews?.length === 0 ? <div className='spinner'><Spinner animation="border" /></div>
+                    : allNews.map(news => <News key={news.url} news={news} />)
             }
         </Row>
     );
